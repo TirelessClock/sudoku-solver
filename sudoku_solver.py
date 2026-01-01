ae1ac37a54a34991ac02e6f803e1757841c05aeb
Abhimanyu Dayal <92638241+TirelessClock@users.noreply.github.com>
2026-01-02 02:15:29 +0530

    improved image classifier

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -90,7 +90,17 @@ def solvePuzzle(grid):
 
     return grid
 
+def is_blank(image):
+    mean, std_dev = cv2.meanStdDev(image)
+    if np.all(std_dev < 10): 
+        return True
+    return False
+
 def read_number(image):
+
+    if is_blank(image):
+        return 0
+
     results = reader.readtext(
         image,
         allowlist='123456789',
@@ -98,7 +108,7 @@ def read_number(image):
     )
 
     if not results:
-        return 0
+        return 7
 
     # If multiple detections, join or pick largest later
     return "".join(results)
@@ -106,8 +116,8 @@ def read_number(image):
 def get_grid():
     image = cv2.imread("image.png")
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    _, image = cv2.threshold(image,160,255,cv2.THRESH_BINARY)#205
-    kernel = np.ones((6,6),np.uint8)
+    _, image = cv2.threshold(image,200,255,cv2.THRESH_BINARY)
+    kernel = np.ones((7,7),np.uint8)
     image = cv2.erode(image, kernel,iterations = 1)
 
     contours, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
@@ -123,7 +133,7 @@ def get_grid():
     for c in range(2, len(contours)):
         area = cv2.contourArea(contours[c])
 
-        if ((0.9*small_sq_area_approx) < area) and (area < (1.1*small_sq_area_approx)):
+        if ((0.8*small_sq_area_approx) < area) and (area < (1.2*small_sq_area_approx)):
             x,y,w,h = cv2.boundingRect(contours[c])
             coordinates.append([x,y,w,h])
 
